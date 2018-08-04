cae3c02f5af4df2be09cc9ed426b105ca4735bde
neovraz <neovrazgmail.com>
2018-08-04 23:52:37 +0530

    REDME

--- script1.sh
+++ script1.sh
@@ -1,4 +1,4 @@
-s#!/bin/bash
+#!/bin/bash
 
 new_user=$1
 password='root'
@@ -9,15 +9,6 @@ sudo adduser --disabled-password --shell /bin/bash --home /home/$new_user --geco
 
 sudo echo "$new_user:$password" | chpasswd
 
-
-#sudo adduser --disabled-password --gecos "" $new_user 
-
-#sudo echo $password | passwd $new_user
-
-#sudo grep -i "$new_user" /etc/shadow | awk -F':' '{ print $2 }'
-
-
-#sudo usermod -a -G sshusers $new_user
 
 sudo su $new_user
 
